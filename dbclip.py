95fffd3ccfb114d3f6100ac3e2f44ab3561afdd2
xxr <xu.xinrui@foxmail.com>
2024-03-01 18:20:45 +0800

    Initial commit

---
+++ dbclip.py
@@ -0,0 +1,187 @@
+import os
+from typing import Any, Mapping
+import yaml, torch
+import torch.nn as nn
+import torch.nn.functional as F
+from datasets import UnifiedFrameSampler, spatial_temporal_view_decomposition
+
+import time
+from torch.cuda.amp import autocast
+from Visual_Prompt import visual_prompt
+from dover import DOVER
+
+import open_clip
+from efficientvideorecognition import model as eff_model
+
+import os 
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+Device = 'cuda' if torch.cuda.is_available() else 'cpu'
+clip_model = open_clip.load_openai_model('ViT-B-16')
+
+
+def read_video(video_path: str, yml_path: str = r"maxvqa.yml", data_set = "universal"):
+    with open(yml_path, "r") as f:
+        opt = yaml.safe_load(f)
+
+        data_option = opt["data"][data_set]["args"]
+
+        temporal_samplers = {}
+        for sample_type, sample_option in data_option["sample_types"].items():
+            if "t_frag" not in sample_option:
+                # resized temporal sampling for TQE in DOVER
+                temporal_samplers[sample_type] = UnifiedFrameSampler(
+                    sample_option["clip_len"], sample_option["num_clips"], sample_option["frame_interval"]
+                )
+            else:
+                # temporal sampling for AQE in DOVER
+                temporal_samplers[sample_type] = UnifiedFrameSampler(
+                    sample_option["clip_len"] // sample_option["t_frag"],
+                    sample_option["t_frag"],
+                    sample_option["frame_interval"],
+                    sample_option["num_clips"],
+                )
+    mean = torch.FloatTensor([123.675, 116.28, 103.53]).reshape(-1,1,1,1)
+    std = torch.FloatTensor([58.395, 57.12, 57.375]).reshape(-1,1,1,1)
+    video_data, _ = spatial_temporal_view_decomposition(
+        video_path, data_option["sample_types"], temporal_samplers,
+    )
+    video_data = {"aesthetic": (video_data["aesthetic"] - mean) / std,
+                  "technical": (video_data["technical"] - mean) / std}
+    return video_data
+
+def encode_text_prompts(tokenizer, prompts,device="cuda"):
+        text_tokens = tokenizer(prompts).to(device)
+        with torch.no_grad():
+            embedding = clip_model.token_embedding(text_tokens)
+            text_features = clip_model.encode_text(text_tokens).float()
+        return text_tokens, embedding, text_features
+
+
+def trunc_normal_(x, mean=0., std=1.):
+    # From https://discuss.pytorch.org/t/implementing-truncated-normal-initializer/4778/12
+    return x.normal_().fmod_(2).mul_(std).add_(mean)
+
+
+class TextEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.transformer = clip_model.transformer
+        self.positional_embedding = clip_model.positional_embedding
+        self.ln_final = clip_model.ln_final
+        self.text_projection = clip_model.text_projection
+        self.dtype = clip_model.transformer.get_cast_dtype()
+        self.attn_mask = None
+
+    def forward(self, prompts, tokenized_prompts):
+        # assert not torch.any(torch.isnan(self.text_projection))
+        x = prompts + self.positional_embedding.type(self.dtype)
+        x = x.permute(1, 0, 2).to(self.dtype)  # NLD -> LND
+        x = self.transformer(x, attn_mask=self.attn_mask)
+        x = x.permute(1, 0, 2)  # LND -> NLD
+        x = self.ln_final(x).type(self.dtype)
+        # x.shape = [batch_size, n_ctx, transformer.width]
+        # take features from the eot embedding (eot_token is the highest number in each sequence)
+        x = x[torch.arange(x.shape[0]), tokenized_prompts.argmax(dim=-1)] @ self.text_projection
+
+        return x
+    
+
+class MLP(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super().__init__()
+        self.in_conv = nn.Conv1d(in_channels, hidden_channels, kernel_size=1)
+        self.out_conv = nn.Conv1d(hidden_channels, out_channels, kernel_size=1)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(0.5)
+        self.apply(self.init_weights)
+
+    def init_weights(self, module):
+        if isinstance(module, nn.Conv1d):
+            module.weight.data.normal_(std=0.5)
+
+    def forward(self, x:torch.tensor):
+        shape = x.shape
+        if x.ndim > 3:
+            x = x.reshape(-1, *shape[-2:])
+        x = x.transpose(-1, -2)  # Reshape input to (batch_size, in_channels, sequence_length)
+        x = self.in_conv(x)
+        x = self.gelu(x)
+        x = self.dropout(x)
+        x = self.out_conv(x)
+        return x.transpose(-1, -2).reshape(*shape[:-1], self.out_conv.out_channels)  # Reshape output back to (batch_size, sequence_length, out_channels)
+
+
+class video_encoder(nn.Module):
+    def __init__(self, video_len = 16):
+        super().__init__()
+        self.temp_fusion = visual_prompt(clip_model.state_dict(), video_len)
+
+
+    def forward(self, video):
+        b, t, c, h, w = video.shape
+
+        images = video.reshape(-1, c, h, w)
+        with torch.no_grad():
+            image_features = clip_model.encode_image(images)
+
+            *_, width = image_features.shape
+            image_features = image_features.reshape(b, t, width)
+
+        image_feature = self.temp_fusion(image_features)
+        image_feature = nn.functional.layer_norm(image_feature, [image_feature.shape[-1]])
+        return image_feature
+
+text_encoder = TextEncoder()
+
+
+class newModel(nn.Module):
+    '''
+    计算图文匹配度和视频质量
+    '''
+    def __init__(self, n_frames=16):
+        super().__init__()
+
+        self.video_encoder_aesthetic = video_encoder(n_frames//2)
+        self.video_encoder_technical = eff_model.EVLTransformer(num_frames=n_frames)
+        self.mlp_tech = MLP(512, 64, 8)
+        self.vqa_head = nn.Linear(16, 1)
+        self.dropout = nn.Dropout()
+        self.ln = nn.LayerNorm([512])
+        
+        # quality_prompt = ['a high quality picture']
+        # with torch.no_grad():
+        #     self.q_token = clip_model.encode_text(open_clip.tokenize(quality_prompt).to(Device)).float()
+
+        # with open(r"/home/data1/xxr/NTIREdataset/aigcc.yml", "r") as f:
+        #     opt = yaml.safe_load(f)
+        # fast_vqa_encoder = DOVER(**opt["model"]["args"])
+        # fast_vqa_encoder.load_state_dict(torch.load("../DOVER-master/pretrained_weights/DOVER.pth"),strict=False)
+        # self.vqa = fast_vqa_encoder.technical_backbone
+
+    def forward(self, aesthetic_video, technical_video, tokens):
+
+        with torch.no_grad():
+        #     batch_size = aesthetic_video.shape[0]
+        #     self.q_batch_token = torch.cat([self.q_token] * batch_size, dim=0)
+            text_feats = clip_model.encode_text(tokens).float()
+        # # 生成query，key和value
+        # query = self.query_proj(self.q_batch_token.unsqueeze(1))
+        # key = self.key_proj(text_feats.unsqueeze(1))
+        # value = self.value_proj(text_feats.unsqueeze(1))
+        # fused_txt = self.attn(query, key, value)[0].squeeze(1)
+            
+        # fused_txt = self.txt_mlp(torch.cat((self.q_batch_token, text_feats), dim=-1))
+        # fused_txt = fused_txt + text_feats
+
+        # with torch.no_grad():
+        #     fastvqa_feature = self.vqa(technical_video.permute(0, 2, 1, 3, 4)).mean(dim=(2,3,4))
+        
+        semantic_feature = self.video_encoder_aesthetic(aesthetic_video)
+        technical_feature = self.video_encoder_technical(technical_video)
+
+        match = torch.einsum('bf,btf -> bt', text_feats, semantic_feature)
+        
+        tech_feat = self.mlp_tech(technical_feature)
+        res = self.vqa_head(torch.cat((match, tech_feat), dim=-1))
+
+        return res
---
+++ train.py
@@ -0,0 +1,424 @@
+import glob
+import os
+import random
+import torch
+from rich.progress import track
+import numpy as np
+import wandb
+from torch.cuda.amp import autocast
+from torch.utils.data import DataLoader, Dataset, random_split
+from scipy.stats import pearsonr, spearmanr
+from copy import deepcopy
+import pandas as pd
+import open_clip
+import dbclip
+import time
+
+import multiprocessing
+try:
+    multiprocessing.set_start_method('spawn')
+except RuntimeError:
+    pass
+
+Device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+class KoNViDdataset(Dataset):
+    def __init__(self, path_to_labelfile: str):
+        super().__init__()
+        with open(path_to_labelfile, 'r') as f:
+            content = f.read()
+            lines = content.splitlines()
+        informations = np.array([line.split(', ') for line in lines])
+        self.video_paths = informations[:, 0]
+        self.labels = torch.from_numpy(informations[:, 3].astype(np.float32))
+
+    def __getitem__(self, index):
+        feat_path = 'E:/features/k/' + self.video_paths[index][-14:-4] + '.pt'
+        if glob.glob(feat_path):
+            video_data = torch.load(feat_path)
+        else:
+            video_data = dbclip.read_video(self.video_paths[index])
+            torch.save(video_data, feat_path)
+        video_tec = video_data["technical"].permute(1, 0, 2, 3).to(device=Device)
+        video_aes = video_data["aesthetic"].permute(1, 0, 2, 3).to(device=Device)
+        label = self.labels[index].to(Device).unsqueeze(0)
+        return video_aes, video_tec, label
+
+    def __len__(self):
+        return len(self.labels)
+
+
+class MaxWelldataset(Dataset):
+    def __init__(self, path_labelfile: str, path_video_dir, video_names):
+        super().__init__()
+        with open(video_names, 'r') as f:
+            content = f.read()
+            lines = content.splitlines()
+        information = np.array([line.split(', ') for line in lines])
+        self.video_names = information[:, 0]
+        self.labels = torch.from_numpy(information[:, 3].astype(np.float32))
+        csv = pd.read_csv(path_labelfile)
+        self.video_dir = path_video_dir
+        self.gts = [csv.iloc[ind].values for ind in range(len(csv))]
+
+    def __getitem__(self, index):
+
+        video_name = self.video_names[index]
+        feat_path = 'E:/features/m/' + video_name + '.pt'
+        if glob.glob(feat_path):
+            video_data = torch.load(feat_path)
+        else:
+            video_data = dbclip.read_video(os.path.join(self.video_dir, video_name))
+            torch.save(video_data, feat_path)
+
+        video_tec = video_data["technical"].permute(1, 0, 2, 3).to(device=Device)
+        video_aes = video_data["aesthetic"].permute(1, 0, 2, 3).to(device=Device)
+        label = torch.tensor(self.gts[index], device=Device, dtype=torch.float32)
+        return video_aes, video_tec, label
+
+    def __len__(self):
+        return len(self.gts)
+        
+
+    def __getitem__(self, index):
+
+        video_name = self.video_names[index]
+        feat_path = 'E:/features/m/' + video_name + '.pt'
+        if glob.glob(feat_path):
+            video_data = torch.load(feat_path)
+        else:
+            video_data = dbclip.read_video(os.path.join(self.video_dir, video_name))
+            torch.save(video_data, feat_path)
+
+        video_tec = video_data["technical"].permute(1, 0, 2, 3).to(device=Device)
+        video_aes = video_data["aesthetic"].permute(1, 0, 2, 3).to(device=Device)
+        label = torch.tensor(self.gts[index], device=Device, dtype=torch.float32)
+        return video_aes, video_tec, label
+
+    def __len__(self):
+        return len(self.gts)
+
+
+class MaxFeatDataset(Dataset):
+    def __init__(self, path_labelfile: str, feat_dir, video_names, mode):
+        '''
+        mode: 'train' or 'val'
+        '''
+        super().__init__()
+        with open(video_names, 'r') as f:
+            content = f.read()
+            lines = content.splitlines()
+        information = np.array([line.split(', ') for line in lines])
+        self.video_names = information[:, 0]
+        self.labels = torch.from_numpy(information[:, 3].astype(np.float32))
+        csv = pd.read_csv(path_labelfile)
+        self.gts = [csv.iloc[ind].values for ind in range(len(csv))]
+        print('loading..')
+        self.aes_feat = torch.load(feat_dir + '/maxvqa_aes_' + mode + '-maxwell.pkl')
+        self.tec_feat = torch.load(feat_dir + '/maxvqa_tech_' + mode + '-maxwell.pkl')
+        print('loaded!')
+
+    def __getitem__(self, index):
+        tec_feat = self.tec_feat[index].to(device=Device)
+        aes_feat = self.aes_feat[index].to(device=Device)
+        label = torch.tensor(self.gts[index], device=Device, dtype=torch.float32)
+        return aes_feat, tec_feat, label
+    
+    def __len__(self):
+        return len(self.gts)
+    
+
+class NTIRE_Dataset(Dataset):
+    def __init__(self, data_dir, mode='training'):
+        '''
+        args:
+            data_dir: 数据集根目录
+            mode: 'training', 'val', 'test'
+        '''
+        super(NTIRE_Dataset, self).__init__()
+        self.video_dir = os.path.join(data_dir, mode)
+        self.data = []
+        self.data_dir = data_dir
+        self.mode = mode
+        label_file = data_dir + '/' + mode + '.txt'
+        if mode == 'training':
+            with open(label_file, 'r', encoding='utf-8') as f:
+                for line in f:
+                    video_path, description, label = line.strip().split('|')
+                    self.data.append((video_path, description, float(label)))
+        else:
+            with open(label_file, 'r', encoding='utf-8') as f:
+                for line in f:
+                    video_path, description = line.strip().split('|')
+                    self.data.append((video_path, description, 0.0))
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        video, description, label = self.data[index]
+        video_path = os.path.join(self.video_dir, video)
+        video_data = dbclip.read_video(video_path, yml_path=r"/home/data1/xxr/NTIREdataset/aigcc.yml")
+
+        a = video_data["aesthetic"].permute(1, 0, 2, 3)
+        t = video_data["technical"].permute(1, 0, 2, 3)
+
+        # if(random.random() > 0.5):
+        #     a = torch.flip(a, [3])
+        #     t = torch.flip(t, [3])
+        
+        if self.mode == 'training':
+            label = torch.tensor(label, dtype=torch.float32).unsqueeze(0)
+            return a, t, description, label
+        else:
+            return a, t, description, video
+
+def rank_loss(y_pred, y):
+    if(y.ndim == 1):
+        y = y.unsqueeze(0)
+    if(y_pred.ndim == 1):
+        y_pred = y_pred.unsqueeze(0)
+    ranking_loss = torch.nn.functional.relu(
+        (y_pred - y_pred.t()) * torch.sign((y.t() - y))
+    )
+    scale = 1 + torch.max(ranking_loss)
+    return (
+            torch.sum(ranking_loss) / y_pred.shape[0] / scale
+    ).float()
+
+
+def plcc_loss(y_pred, y):
+    sigma_hat, m_hat = torch.std_mean(y_pred, unbiased=False)
+    y_pred = (y_pred - m_hat) / (sigma_hat + 1e-8)
+    sigma, m = torch.std_mean(y, unbiased=False)
+    y = (y - m) / (sigma + 1e-8)
+    loss0 = torch.nn.functional.mse_loss(y_pred, y) / 4
+    rho = torch.mean(y_pred * y)
+    loss1 = torch.nn.functional.mse_loss(rho * y_pred, y) / 4
+    return ((loss0 + loss1) / 2).float()
+
+
+
+def train(model, train_set, val_set = None):
+    train_loader  = DataLoader(train_set, 16, shuffle=True, num_workers=2, pin_memory=True)
+    val_loader    = DataLoader(val_set, 16, num_workers=2, pin_memory=True)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.0006)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 30, eta_min=0.00005)
+
+    wandb.init(
+    project="aicg-vqa",
+    config={
+        
+        "architecture": "newModel with fastvqa",
+        "dataset": "aicg-vqa",
+        "epochs": 30,
+    }
+    )
+    best_mcc = 0.78
+    # torch.autograd.set_detect_anomaly(True)
+    for epoch in range(30):
+        
+        print('epoch:', epoch+1, '\t', 'lr:', scheduler.get_last_lr()[0])
+        wandb.log({'epoch': epoch ,'lr': scheduler.get_last_lr()[0]})
+        
+        # train
+        model.train(True)
+        
+        epoch_loss = 0.
+        for a, t, desc, gt in track(train_loader, description='train'):
+            torch.cuda.empty_cache()
+            optimizer.zero_grad()
+
+            # prompts = desc
+            quality_prompt = "a high quality picture of "
+            prompts = [quality_prompt + des  for des in desc]
+
+            tokens = open_clip.tokenize(prompts).to(Device)
+            a = a.to(Device)
+            t = t.to(Device)
+            gt=gt.to(Device)
+
+            with autocast():
+                res = model(a, t, tokens)
+                
+                all_loss = 0.
+                all_loss = plcc_loss(res[:, 0], gt[:, 0])
+                wandb.log({'loss': all_loss})
+
+            # with torch.autograd.detect_anomaly():
+            all_loss.backward(retain_graph=True)
+            optimizer.step()
+
+            epoch_loss += all_loss.detach().cpu()
+            
+        torch.save(model.state_dict(), 'pretrain/now.pt')
+        wandb.log({'epoch loss': epoch_loss, 'epoch': epoch})
+
+        if val_set is None:
+            torch.save(model.state_dict(), f'pretrain/now_{epoch}.pt')
+
+        else:
+            # validate
+            model.eval() 
+            val_prs, val_gts = [], []
+            for a, t, desc, gt in track(val_loader,description=' val '):
+                # prompts = [prompt + des for prompt in base_prompts for des in desc]
+                prompts = desc
+                tokens = open_clip.tokenize(prompts).to(Device)
+                a = a.to(Device)
+                t = t.to(Device)
+                gt=gt.to(Device)
+                with torch.no_grad() and autocast():
+                    res = model(a, t, tokens)
+                    
+                val_prs.extend(list(res.detach().cpu().numpy()))
+                val_gts.extend(list(gt.detach().cpu().numpy()))
+                torch.cuda.empty_cache()
+            val_prs = np.stack(val_prs, 0).squeeze()
+            val_gts = np.stack(val_gts, 0).squeeze()
+            mean_cc, plcc, srcc = 0, 0, 0
+            print('\tsrcc\t\t\tplcc')
+            if val_prs.ndim > 1:
+                for i in range(16):
+                    srcc = spearmanr(val_prs[:, i], val_gts[:, i])[0]
+                    plcc = pearsonr(val_prs[:, i].squeeze(), val_gts[:, i].squeeze())[0]
+                    print(srcc, '\t', plcc)
+                    mean_cc = max(mean_cc, (plcc+srcc)/2)
+                    wandb.log({f'plcc_{i}': plcc, f'srcc_{i}': srcc, 'epoch': epoch})
+            else:
+                srcc, plcc = spearmanr(val_prs, val_gts)[0], pearsonr(val_prs, val_gts)[0]
+                wandb.log({'plcc': plcc, 'srcc': srcc, 'epoch': epoch})
+                print(srcc, '\t', plcc)
+                mean_cc = (plcc+srcc)/2
+            if mean_cc > best_mcc:
+                torch.save(model.state_dict(), "pretrain/best.pt")
+                print('saved!')
+                best_mcc = mean_cc
+        
+        scheduler.step()
+
+
+def test(model, data):
+    # validate
+    model.eval() 
+    loader = DataLoader(data, 4)
+    val_prs, val_gts = [], []
+    for a, t, gt in track(loader,description=' val '):
+        with torch.no_grad() and autocast():
+            res = model(a, t)
+            print(res)
+        val_prs.extend(list(res.detach().cpu().numpy()))
+        val_gts.extend(list(gt.detach().cpu().numpy()))
+        torch.cuda.empty_cache()
+    val_prs = np.stack(val_prs, 0).squeeze()
+    val_gts = np.stack(val_gts, 0).squeeze()
+    all_plcc, plcc, srcc = 0, 0, 0
+    print('\tsrcc\t\t\tplcc')
+    if val_prs.ndim > 1:
+        for i in range(16):
+            srcc = spearmanr(val_prs[:, i], val_gts[:, i])[0]
+            plcc = pearsonr(val_prs[:, i].squeeze(), val_gts[:, i].squeeze())[0]
+            print(srcc, '\t', plcc)
+            all_plcc = max(all_plcc, plcc)
+    else:
+        srcc, plcc = spearmanr(val_prs, val_gts)[0], pearsonr(val_prs, val_gts)[0]
+        print(srcc, '\t', plcc)
+        all_plcc = plcc
+
+
+def generate_result(model, test_data):
+    import csv
+    model.eval()
+    val_prs, vid_names = [], []
+    val_loader = DataLoader(test_data, 16)
+    
+    Runtime = 0
+    for a, t, desc, name in track(val_loader,description='test '):
+        
+        prompts = desc
+        tokens = open_clip.tokenize(prompts).to(Device)
+        with torch.no_grad() and autocast():
+            t_begin = time.time()
+            res = model(a, t, tokens)
+            t_dur = time.time() - t_begin
+            Runtime += t_dur
+        val_prs.extend(res.squeeze(-1).detach().cpu().tolist())
+        vid_names.extend(list(name))
+        torch.cuda.empty_cache()
+
+    with open(r"/home/data1/xxr/NTIREdataset\output.txt", 'w', newline='',encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for pr, name in zip(val_prs, vid_names):
+            writer.writerow([name, pr])
+    
+    with open(r"/home/data1/xxr/NTIREdataset\readme.txt", 'w', encoding='utf-8') as file:
+        file.write(f"Runtime per video [s] : " + str(Runtime/len(vid_names)) + '\n'
+                    "CPU[1] / GPU[0] : 0" + '\n' + 
+                    "Extra Data [1] / No Extra Data [0] : 0" + '\n' +
+                    "LLM [1] / No LLM [0] : 0" + '\n' + 
+                    'Other description : ' + 'based on open_clip.' + '\n')
+
+
+
+if __name__ == '__main__':
+    def main():
+        generator42 = torch.Generator().manual_seed(42)
+        # max_files = r'/home/data1/xxr/NTIREdataset\maxvqa'
+        # maxdir = max_files + r'\datasets\MaxWell'
+        # max_train_label = max_files + r'\ExplainableVQA\MaxWell_train.csv'
+        # max_val_label   = max_files + r'\ExplainableVQA\MaxWell_val.csv'
+        # max_test_video  = max_files + r"\ExplainableVQA\examplar_data_labels\MaxWell\test_labels.txt"
+        # max_train_video = max_files + r"\ExplainableVQA\examplar_data_labels\MaxWell\train_labels.txt"
+        # Kset = KoNViDdataset('datasets\KoNViD.txt')
+        # K_train_set, K_val_set = random_split(Kset, (0.8, 0.2), generator42)
+        # M_train_set = MaxWelldataset(max_train_label, maxdir, max_train_video)
+        # M_val_set = MaxWelldataset(max_val_label, maxdir, max_test_video)
+
+        aigc_trainset = NTIRE_Dataset(r'/home/data1/xxr/NTIREdataset')
+        aigc_valset = NTIRE_Dataset(r'/home/data1/xxr/NTIREdataset', mode='val')
+
+        positive_descs = ["high quality", "good content", "organized composition", "vibrant color",
+                        "contrastive lighting", "consistent trajectory",
+                        "good aesthetics",
+                        "sharp", "in-focus", "noiseless", "clear-motion", "stable", "well-exposed",
+                        "original", "fluent", "clear",
+                        ]
+
+        negative_descs = ["low quality", "bad content", "chaotic composition", "faded color", "gloomy lighting",
+                        "incoherent trajectory",
+                        "bad aesthetics",
+                        "fuzzy", "out-of-focus", "noisy", "blurry-motion", "shaky", "poorly-exposed",
+                        "compressed", "choppy", "severely degraded",
+                        ]
+        context = "X"
+        pos_prompts = [f"a {context} {desc} picture" for desc in positive_descs]
+        neg_prompts = [f"a {context} {desc} picture" for desc in negative_descs]
+        Mprompts = pos_prompts + neg_prompts
+        base_prompts = [f"a high quality picture of ", f"a low quality picture of "]
+        prompts = [f'a {context} high quality picture', f'a {context} low quality picture']
+        tokenizer = open_clip.get_tokenizer("ViT-B-32")
+        text_tokens, embedding, text_feats = dbclip.encode_text_prompts(tokenizer, prompts, device=Device)
+        # model_q = dbclip.myModel(text_tokens, embedding, n_frames=16).to(Device)
+        model = dbclip.newModel(n_frames=16).to(Device)
+        
+        # model_ema = dbclip.myModel(text_tokens, embedding).to(Device)
+
+        # test(model, K_val_set)
+        # text_encoder = dbclip.TextEncoder()
+        # model.initialize_inference()
+        # Mtrain_set = MaxFeatDataset(max_train_label, 'features', max_train_video, mode='train')
+        # Mval_set =  MaxFeatDataset(max_train_label, 'features', max_test_video, mode='val')
+        # train(model, Mtrain_set, Mval_set,)
+        # train(model, M_train_set, M_val_set,)
+        # train(model, K_train_set, K_val_set)
+        
+        # state = torch.load(r'pretrain/now.pt')
+        # model.load_state_dict(state, strict=True)
+        aigc_trainset, aigc_valset = random_split(aigc_trainset, (0.9, 0.1), generator42)
+        train(model, aigc_trainset, aigc_valset)
+        
+        
+        # generate_result(model, aigc_valset)
+
+    main()
